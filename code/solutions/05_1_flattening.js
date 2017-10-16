c36766f4717aa0802a75f6437cbe3e42a01d7785
Aaron Burrows <me@aaronburrows.com>
2017-10-16 17:01:11 -0500

    add solution to 5-1

--- code/solutions/05_1_flattening.js
+++ code/solutions/05_1_flattening.js
@@ -1,7 +1,19 @@
 var arrays = [[1, 2, 3], [4, 5], [6]];
 
-console.log(arrays.reduce(function(flat, current) {
-  return flat.concat(current);
-}, []));
+function flatten(arrays) {
+  var out = [];
+  arrays.forEach (function(arr){
+    for (i = 0; i < arr.length; i++) {
+      out.push(arr[i]);
+    }
+  });
+  return out;
+  //console.log(reduce(arrays[0]));
+}
+
+console.log(arrays.reduce(
+  function(flat, current) {
+  	return flat.concat(current);
+},[]));
 
 // → [1, 2, 3, 4, 5, 6]
